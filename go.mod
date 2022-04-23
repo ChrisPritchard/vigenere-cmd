41c645666695a391578ac2b1ffec590447d0e149
Chris Pritchard <chris@grislygrotto.nz>
2022-04-23 14:43:51 +1200

    sketched out process

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChrisPritchard/vigenere-cmd
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// read input
+	// read key
+	// rev option (enc)
+
+	// brute key (known phrase)
+	// keylen
+	// kewwords
+
+	// construct offsets
+	// perform vigenere
+	// write out
+}
